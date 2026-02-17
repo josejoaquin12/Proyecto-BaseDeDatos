65dcefb80a50fc66d451c6e139e532208d1d0683
jesuscosio268367-arch <jesus.cosio268367@potros.itson.edu.mx>
2026-02-17 12:11:29 -0600

    Add files via upload

---
+++ Proyecto01/src/main/java/com/mycompany/proyecto01/Proyecto01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto01;
+
+/**
+ *
+ * @author elgps
+ */
+public class Proyecto01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
