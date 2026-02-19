9fb93a117a584a2107027b7278066a74136e104b
AndreaLaraOsuna <lara.carmen.cb37@gmail.com>
2026-02-19 00:56:19 -0700

    BO y DAO de la transferencia
    según yo esta bien, denme correcciones

--- Proyecto01/src/main/java/org/itson/proyecto01/negocio/ITransferenciasBO.java
+++ Proyecto01/src/main/java/org/itson/proyecto01/negocio/ITransferenciasBO.java
@@ -4,11 +4,18 @@
  */
 package org.itson.proyecto01.negocio;
 
+import org.itson.proyecto01.dtos.NuevaTransferenciaDTO;
+import org.itson.proyecto01.entidades.Transferencia;
+
 /**
  *
  * @author calo2
  */
 public interface ITransferenciasBO {
     
+    public abstract Transferencia realizarTransferencia(
+            NuevaTransferenciaDTO nuevaTransferencia
+    )throws NegocioException;
+        
     
 }
