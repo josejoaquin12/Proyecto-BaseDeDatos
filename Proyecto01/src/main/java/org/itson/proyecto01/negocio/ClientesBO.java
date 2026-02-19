e610304c995f025620251042ff663ae586556dc4
jesuscosio268367-arch <jesus.cosio268367@potros.itson.edu.mx>
2026-02-18 18:58:04 -0600

    Avance clientesBO, iClientesBO
    Avance en clientesBO, iClientesBO y creacion de IClientesDAO

--- Proyecto01/src/main/java/org/itson/proyecto01/negocio/ClientesBO.java
+++ Proyecto01/src/main/java/org/itson/proyecto01/negocio/ClientesBO.java
@@ -4,10 +4,59 @@
  */
 package org.itson.proyecto01.negocio;
 
+import java.time.LocalDateTime;
+import org.itson.proyecto01.dtos.NuevoClienteDTO;
+import org.itson.proyecto01.entidades.Cliente;
+import org.itson.proyecto01.persistencia.IClientesDAO;
+
 /**
  *
- * @author elgps
+ * @author Jesus Omar
  */
-public class ClientesBO {
+public class ClientesBO implements IClientesBO {
+    
+    private final IClientesDAO clientesDAO;
+    
+    public ClientesBO(IClientesDAO clientesDAO){
+        this.clientesDAO = clientesDAO;
+    }
+
+    @Override
+    public Cliente crearCliente(NuevoClienteDTO nuevoCliente)throws NegocioException{
+        // Limite de edad para validar que el cliente sea mayor de edad
+        LocalDateTime edadMinima = LocalDateTime.now().minusYears(18);
+        // Validaciones
+        if(nuevoCliente.getNombres() == null){
+            throw new NegocioException("El nombre no puede estar vacio", null);
+        }
+        if(nuevoCliente.getApellidoP() == null){
+            throw new NegocioException("El apellido paterno no puede estar vacio", null);
+        }
+        if(nuevoCliente.getApelidoM() == null){
+            throw new NegocioException("El apellido materno no puede estar vacio", null);
+        }
+        if(nuevoCliente.getFechaNacimiento() == null){
+            throw new NegocioException("La fecha de nacimiento no puede estar vacia", null);
+        }
+        if(nuevoCliente.getContrasenia() == null){
+            throw new NegocioException("La contrasena no puede estar vacia", null);
+        }
+        if(nuevoCliente.getNombres().length() > 100){
+            throw new NegocioException("El nombre es demasiado largo", null);
+        }
+        if(nuevoCliente.getApellidoP().length() > 100){
+            throw new NegocioException("El apellido es demasiado largo", null);
+        }
+        if(nuevoCliente.getApelidoM().length() > 100){
+            throw new NegocioException("El apellido es demasiado largo", null);
+        }
+        if(nuevoCliente.getFechaNacimiento().isAfter(edadMinima)){
+            throw new NegocioException("La edad minima para abrir una cuenta es de 18", null);
+        }
+        if(nuevoCliente.getContrasenia().length() < 6){
+            throw new NegocioException("La contrasena debe contener minimo 6 caracteres", null);
+        }
+        
+    }
     
 }
---
+++ Proyecto01/src/main/java/org/itson/proyecto01/negocio/IClientesBO.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.itson.proyecto01.negocio;
+
+import org.itson.proyecto01.dtos.NuevoClienteDTO;
+import org.itson.proyecto01.entidades.Cliente;
+
+/**
+ *
+ * @author Jesus Omar
+ */
+public interface IClientesBO {
+    
+    public abstract Cliente crearCliente(NuevoClienteDTO nuevoCliente)throws NegocioException;
+    
+}
